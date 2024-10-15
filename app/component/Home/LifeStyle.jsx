7b424c8623509720c0bfa4c227290e3461953f89
Sanket0798 <rathodsanket2022@gmail.com>
2024-10-15 11:53:44 +0530

    Initial commit of LifeStyle Section in HomePage.

--- app/component/Home/LifeStyle.jsx
+++ app/component/Home/LifeStyle.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+
+const lifestyleCategories = [
+  { name: "Beach Lovers", image: "/assets/HomePage/LifeStyle/Img1.jpg" },
+  {
+    name: "Street Food Lovers",
+    image: "/assets/HomePage/Locality/Andheri.jpg",
+  },
+  { name: "Movie Buffs", image: "/assets/HomePage/Locality/Andheri.jpg" },
+  { name: "Scholars", image: "/assets/HomePage/Locality/Andheri.jpg" },
+  {
+    name: "Spiritual Wellness Seekers",
+    image: "/assets/HomePage/Locality/Andheri.jpg",
+  },
+  {
+    name: "Corporate Warriors",
+    image: "/assets/HomePage/Locality/Andheri.jpg",
+  },
+  { name: "Shopoholics", image: "/assets/HomePage/Locality/Andheri.jpg" },
+  { name: "Party Animals", image: "/assets/HomePage/Locality/Andheri.jpg" },
+  { name: "Daily Commuters", image: "/assets/HomePage/Locality/Andheri.jpg" },
+  { name: "Fitness Freaks", image: "/assets/HomePage/Locality/Andheri.jpg" },
+  { name: "Food Connoisseurs", image: "/assets/HomePage/Locality/Andheri.jpg" },
+  { name: "Caregivers", image: "/assets/HomePage/Locality/Andheri.jpg" },
+];
 
 const LifeStyle = () => {
   return (
-    <div>LifeStyle</div>
-  )
-}
+    <section className="w-1200 h-[942px] mx-auto flex items-center justify-between">
+    </section>
+  );
+};
 
-export default LifeStyle
+export default LifeStyle;
