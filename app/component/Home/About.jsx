b8e6777530a02c180f5b36c7a55ca762213b0981
Sanket0798 <rathodsanket2022@gmail.com>
2024-10-16 21:02:06 +0530

    About Section has been revised as per responsive code.

--- app/component/Home/About.jsx
+++ app/component/Home/About.jsx
@@ -5,24 +5,24 @@ import { aboutContent } from "@/app/utils/Data";
 
 const About = () => {
   return (
-    <div className="w-full h-[744px] bg-[#EDDBDB] py-[60px]">
-      <section className="w-1200 mx-auto flex flex-row items-start justify-between h-full">
+    <div className="w-[360px] md:w-[768px] lg:w-full h-[810px] md:h-[744px] bg-[#EDDBDB] py-[20px] md:py-[60px] px-[10px] md:px-[20px] lg:px-0">
+      <section className="w-1200 mx-auto flex flex-col md:flex-row items-start justify-between h-full">
         <div className="flex flex-col items-start">
-          <p className="text-black font-extrabold text-[42px] w-[575px] leading-[50px]">
+          <p className="text-black font-extrabold md:text-[42px] w-[340px] md:w-[575px] md:leading-[50px] text-[35px] leading-[40px]">
             Reduce your rental expense with{" "}
             <span className="text-[#6B31E7]">GetSetHome</span>
           </p>
           {aboutContent.map(({ title, desc }, index) => (
             <div key={index} className="w-[575px]">
-              <h4 className="text-[24px] text-black font-extrabold mt-[40px] mb-[15px] h-[28px]">
+              <h4 className="text-[20px] md:text-[24px] text-black font-extrabold mt-[40px] mb-[15px] h-[28px]">
                 {title}
               </h4>
-              <p className="text-[14px] text-[#555] leading-[25px]">{desc}</p>
+              <p className="text-[12px] md:text-[14px] text-[#555] leading-[25px] w-[340px] md:w-full">{desc}</p>
             </div>
           ))}
         </div>
 
-        <div className="flex flex-row w-[575px] h-full">
+        <div className="hidden md:flex flex-row w-[575px] h-full">
           <div className="relative w-full h-screen">
             {[
               {
