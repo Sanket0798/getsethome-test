4b90bf5ee724833753e14bd90b830bb7dc3e9f51
Sanket0798 <rathodsanket2022@gmail.com>
2024-10-17 13:01:22 +0530

    Services Section has been modified as per responsive design.

--- app/component/Home/Working.jsx
+++ app/component/Home/Working.jsx
@@ -5,7 +5,7 @@ import { workingContent } from "@/app/utils/Data";
 
 const Working = () => {
   return (
-    <section className="w-auto md:w-[768px] lg:w-1200 mx-auto h-[644px] py-[20px] md:py-[60px] px-[10px] md:px-0">
+    <section className="w-auto md:w-[768px] lg:w-1200 mx-auto h-auto md:h-[644px] py-[20px] md:py-[60px] px-[10px] md:px-0">
       <div className="flex flex-col md:flex-row gap-y-[20px] md:gap-y-0 w-auto md:w-[768px] lg:w-full h-full justify-between">
         <Image
           width={587}
@@ -15,7 +15,7 @@ const Working = () => {
           className="w-auto h-[340px] md:w-[587px] md:h-[538px]"
         />
 
-        <div className="w-auto md:w-[587px] h-[524px] flex flex-col items-start">
+        <div className="w-auto md:w-[587px] h-auto md:h-[524px] flex flex-col items-start">
           <h2 className="font-extrabold md:text-[42px] text-black text-[35px]">
             How it <span className="text-[#6B31E7]">works</span>?
           </h2>
