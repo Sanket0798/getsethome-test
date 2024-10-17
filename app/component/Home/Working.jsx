bacce698bc0f3fcd802e76b19be9233f56bc320b
Sanket0798 <rathodsanket2022@gmail.com>
2024-10-17 12:48:45 +0530

    Minor Changes.

--- app/component/Home/Working.jsx
+++ app/component/Home/Working.jsx
@@ -5,17 +5,17 @@ import { workingContent } from "@/app/utils/Data";
 
 const Working = () => {
   return (
-    <section className="w-1200 mx-auto h-[644px] py-[20px] md:py-[60px] px-[10px] md:px-0">
-      <div className="flex flex-col gap-y-[20px] md:gap-y-0 md:flex-row w-full h-full justify-between">
+    <section className="w-auto md:w-[768px] lg:w-1200 mx-auto h-[644px] py-[20px] md:py-[60px] px-[10px] md:px-0">
+      <div className="flex flex-col md:flex-row gap-y-[20px] md:gap-y-0 w-auto md:w-[768px] lg:w-full h-full justify-between">
         <Image
           width={587}
           height={538}
           alt=""
           src="/assets/HomePage/Working/Background.png"
-          className="w-[340px] h-[290px] md:w-[587px] md:h-[538px]"
+          className="w-auto h-[340px] md:w-[587px] md:h-[538px]"
         />
 
-        <div className="w-[340px] md:w-[587px] h-[524px] flex flex-col items-start">
+        <div className="w-auto md:w-[587px] h-[524px] flex flex-col items-start">
           <h2 className="font-extrabold md:text-[42px] text-black text-[35px]">
             How it <span className="text-[#6B31E7]">works</span>?
           </h2>
